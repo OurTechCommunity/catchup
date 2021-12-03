8a9178eb455b372cc20aa4dc1b4ddc106ff7d020
Harsh Kapadia <harshgkapadia@gmail.com>
2021-12-03 19:16:56 +0530

    :sparkles: feat: Add burger menu

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,23 @@
+const ul = document.querySelector(".navbar-ul");
+const burgerMenu = document.querySelector(".burger-menu");
 const errorDiv = document.querySelector(".error-msg");
 
+// For error message
 window.addEventListener("load", () => {
 	if (!isCatchUpOn()) showMessage();
 });
 
+// For burger menu
+burgerMenu.addEventListener("click", (event) => {
+	ul.classList.toggle("hide-burger-menu");
+	event.stopPropagation();
+});
+
+window.addEventListener("click", () => {
+	ul.classList.add("hide-burger-menu");
+});
+
+// Utility functions
 function isCatchUpOn() {
 	const paramString = window.location.search;
 
