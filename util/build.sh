5e086c1af51ecda207b8c0a9d9303932cc1c7262
Kartik Soneji <kartiksoneji@rocketmail.com>
2022-08-08 06:30:48 +0530

    :hammer: refactor: Cleaner way of parsing catchup numbers

--- util/build.sh
+++ util/build.sh
@@ -30,25 +30,12 @@ sed -i -e 's/<img/<img loading="lazy"/g' "${script_dir}/../public/html/summary/c
 # Build individual summary pages
 for path in "${script_dir}/../summary/sessions/"*; do
 	if [ -d "${path}" ]; then
-		IFS="/";
-		read -ra arr <<< "${path}";
-
-		arr_length=${#arr[@]};
-		catchup_number=${arr[arr_length-1]};
-
-		if [ ${catchup_number:0:2} == "00" ]; then
-			catchup_display_number=${catchup_number:2:1};
-		elif [ ${catchup_number:0:1} == "0" ]; then
-			catchup_display_number=${catchup_number:1:2};
-		else
-			catchup_display_number=catchup_number;
-		fi;
+		catchup_number=${path##*/};
+		printf -v "catchup_display_number" "%.0f" "${catchup_number}";
 
 		asciidoctor "${script_dir}/../summary/individual-summary.adoc" -a webfonts! -o "${script_dir}/../public/html/summary/${catchup_number}.html" -a catchup_number=${catchup_number} -a catchup_display_number=${catchup_display_number};
 
 		sed -i -e "s/<img/<img loading=\"lazy\"/g" "${script_dir}/../public/html/summary/${catchup_number}.html"; # Lazy load images
-
-		unset IFS;
 	fi;
 done;
 
