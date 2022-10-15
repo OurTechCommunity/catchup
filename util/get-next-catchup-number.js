f9e3e4f64274a69c488e358395284cc2b65b4a52
Kaustubh Khavnekar <kaustubhkhavnekar@gmail.com>
2022-10-14 21:46:41 -0400

    :heavy_check_mark: feat: Format files with prettier (#20)

--- util/get-next-catchup-number.js
+++ util/get-next-catchup-number.js
@@ -1,6 +1,6 @@
 const catchupNumber = require("./catchup-number");
 
 catchupNumber
-    .getCatchupNumber(true, true)
-    .then(console.log)
-    .catch(console.error);
+	.getCatchupNumber(true, true)
+	.then(console.log)
+	.catch(console.error);
