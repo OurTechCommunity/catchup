ab2816e9a6476a3b969fd0faca5b4d17a95ca1a3
Kartik Soneji <kartiksoneji@rocketmail.com>
2023-07-15 21:22:10 +0530

    :bug: fix: get seconds waiting script

--- util/get-seconds-until-ist-time.js
+++ util/get-seconds-until-ist-time.js
@@ -1,6 +1,6 @@
 const time = process.argv[2];
-const [hours, minutes] = time.split(":");
-if (!time || !hours || !minutes) {
+const [hours, minutes] = time.split(":").map((d) => parseInt(d));
+if (!time || !isFinite(hours) || !isFinite(minutes)) {
 	console.error(`invalid timestamp: ${time}`);
 	process.exit(1);
 }
@@ -12,7 +12,12 @@ let now = new Date(
 );
 
 let date = new Date(now);
+date.setHours(hours);
+date.setMinutes(minutes);
+date.setSeconds(0);
 
 let seconds = Math.floor((date.getTime() - now.getTime()) / 1000);
 if (seconds < 0) seconds = 0;
 console.log(seconds);
+
+console.warn(`waiting for ${seconds} till ${time}`);
